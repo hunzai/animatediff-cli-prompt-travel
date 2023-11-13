3d7d04b0f4476e4b1da738f15649de415d1a2cd1
Shawaiz <shawaiz.shah@lhg.lovehoneygroup.com>
2023-11-13 01:32:14 +0100

    Add update/download routine to Animate class

--- Animate.py
+++ Animate.py
@@ -1,16 +1,18 @@
-import subprocess
-import sys
+import json
 import os
 import shutil
-from pip._internal.commands import install
+import subprocess
+import sys
 
+import git
 import google.colab
 from google.colab import drive
 from openai import OpenAI
-import git
+from pip._internal.commands import install
+
 
 class Animate:
-  def __init__(self, config_json_path):
+  def __init__(self, config_json_path=None):
     # init config
     self.config_json_path = None
     self.config = None
@@ -18,6 +20,7 @@ class Animate:
     # init vars
     self.head_prompt = None
     self.ref_image = None
+    self.controlnet_images_path = None
     self.use_dalle_ref_image = False
     self.w = 720
     self.h=720
@@ -44,14 +47,24 @@ class Animate:
     else:
       self.REPO_PATH_PARENT = os.getcwd()
 
-    # init repo var on fs
-    self.init_cliper()
-
     # build animate-diff src
     self.build_animate_diff()
 
+    # init repo var on fs
+    self.init_cliper()
+
+    # setup models
+    self.setup_models()
+
     # init openai client
     self.openai_client = OpenAI()
+
+    # update controlnet_image path
+    self.controlnet_images_path = os.path.join(
+        self.REPO_PATH,
+        "data",
+        "controlnet_image"
+    )
 
 
   #
@@ -154,6 +167,12 @@ class Animate:
   def get_animatediff_git_repo(self):
       print("Cloning animatediff repo...")
 
+      #
+      self.REPO_PATH =os.path.join(
+          self.REPO_PATH_PARENT,
+          'animatediff-cli-prompt-travel'
+      )
+
       # clearup repo dir
       shutil.rmtree(self.REPO_PATH, ignore_errors=True)
 
@@ -186,11 +205,6 @@ class Animate:
     print(subprocess_out.stderr)
 
   def init_cliper(self):
-      self.REPO_PATH =os.path.join(
-          self.REPO_PATH_PARENT,
-          'animatediff-cli-prompt-travel'
-      )
-
       # add helpers to module
       PATH_HELPER_CLIPER = os.path.join(
           self.REPO_PATH,
@@ -209,3 +223,177 @@ class Animate:
       #
       import cliper
       self.cliper = cliper
+
+  def setup_models(self):
+      # download models to central model repository
+      self.download_models()
+
+      # copy models from central mdoel repository to cloned repository
+      self.copy_models_to_repo()
+
+
+  # downloads models to [REPO Parent]/models e.g. '/content/drive/MyDrive/AI/models
+  def download_models(self):
+      print("Downloading models")
+
+      #
+      subprocess.run([
+          "apt", "-y", "install", "-qq", "aria2"
+      ])
+
+      #
+      MODEL_REPO_PATH= os.path.join(
+          self.REPO_PATH_PARENT, "models"
+      )
+
+      #
+      PATH_HUGGING_FACE = os.path.join(MODEL_REPO_PATH, "huggingface")
+      PATH_HUGGING_FACE_SD_V15 = os.path.join(PATH_HUGGING_FACE, "v1-5-pruned-emaonly.ckpt")
+
+      #
+      PATH_MOTION_MODULE = os.path.join(MODEL_REPO_PATH, "motion-module")
+      PATH_MOTION_MODULE_SD = os.path.join(PATH_MOTION_MODULE, "mm_sd_v15_v2.ckpt")
+
+      PATH_SD = os.path.join(MODEL_REPO_PATH, "sd")
+      PATH_SD_MISTOON = os.path.join(PATH_SD, "mistoonAnime_v20.safetensors")
+      PATH_SD_DREAM = os.path.join(PATH_SD, "dreamshaper.safetensors")
+
+      PATH_DWPose = os.path.join(MODEL_REPO_PATH, "DWPose")
+      PATH_DWPose_DW11 = os.path.join(PATH_DWPose, "dw-ll_ucoco_384.onnx")
+
+      #
+      if not os.path.exists(PATH_HUGGING_FACE) or not os.path.isfile(PATH_HUGGING_FACE_SD_V15):
+        #
+        subprocess.run([
+            "aria2c", "--console-log-level=error", "-c", "-x", "16", "-k", "1M", "https://huggingface.co/runwayml/stable-diffusion-v1-5", "-d", str(PATH_HUGGING_FACE), "-o"
+        ])
+        # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/runwayml/stable-diffusion-v1-5 -d $MODEL_PATH/huggingface -o
+        print("RUNWAY SD1.5 download success (not found earlier)")
+
+      #
+      if not os.path.exists(PATH_MOTION_MODULE) or not os.path.isfile(PATH_MOTION_MODULE_SD):
+        #
+        subprocess.run([
+            "aria2c", "--console-log-level=error", "-c", "-x", "16", "-k", "1M", "https://huggingface.co/guoyww/animatediff/blob/main/mm_sd_v15_v2.ckpt", "-d", str(PATH_MOTION_MODULE), "-o", "mm_sd_v15_v2.ckpt"
+        ])
+        # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/camenduru/AnimateDiff/resolve/main/mm_sd_v15_v2.ckpt -d $MODEL_PATH/motion-module -o mm_sd_v15_v2.ckpt
+        print("MM SD animatediff download success (not found earlier)")
+
+      #
+      if not os.path.exists(PATH_DWPose) or not os.path.isfile(PATH_DWPose_DW11):
+        subprocess.run([
+            "aria2c", "--console-log-level=error", "-c", "-x", "16", "-k", "1M", "https://huggingface.co/yzd-v/DWPose/blob/main/dw-ll_ucoco_384.onnx", "-d", str(PATH_DWPose), "-o", "dw-ll_ucoco_384.onnx"
+        ])
+        # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/yzd-v/DWPose/blob/main/dw-ll_ucoco_384.onnx -d $MODEL_PATH/DWPose -o dw-ll_ucoco_384.onnx
+        print("DWPose 11 download success (not found earlier)")
+
+      #
+      if not os.path.exists(PATH_SD) or not os.path.isfile(PATH_SD_MISTOON):
+        subprocess.run([
+            "aria2c", "--console-log-level=error", "-c", "-x", "16", "-k", "1M", "https://civitai.com/api/download/models/108545", "-d", str(PATH_SD), "-o", "mistoonAnime_v20.safetensors"
+        ])
+        # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://civitai.com/api/download/models/108545 -d $MODEL_PATH/sd -o mistoonAnime_v20.safetensors
+        print("Mistoon download success (not found earlier)")
+
+      #
+      if not os.path.exists(PATH_SD) or not os.path.isfile(PATH_SD_DREAM):
+        subprocess.run([
+            "aria2c", "--console-log-level=error", "-c", "-x", "16", "-k", "1M", "https://civitai.com/api/download/models/128713", "-d", str(PATH_SD), "-o", "dreamshaper.safetensors"
+        ])
+        # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M  https://civitai.com/api/download/models/128713 -d $MODEL_PATH/sd -o dreamshaper.safetensors
+        print("Dreamshaper download success (not found earlier)")
+
+      # !apt -y install -qq aria2
+      # MODEL_PATH='/content/drive/MyDrive/AI/animatediff-cli-prompt-travel/data/models'
+      # # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/camenduru/AnimateDiff/resolve/main/mm_sd_v15_v2.ckpt -d $MODEL_PATH/motion-module -o mm_sd_v15_v2.ckpt
+      # # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/runwayml/stable-diffusion-v1-5 -d $MODEL_PATH/huggingface -o
+      # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/camenduru/AnimateDiff/resolve/main/mm_sd_v15_v2.ckpt -d $MODEL_PATH/motion-module -o mm_sd_v15_v2.ckpt
+      # !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://civitai.com/api/download/models/108545 -d $MODEL_PATH/sd -o mistoonAnime_v20.safetensors
+
+  def copy_models_to_repo(self):
+      print("Copying models to repo")
+
+      #
+      MODEL_REPO_PATH= os.path.join(
+          self.REPO_PATH_PARENT, "models"
+      )
+      MODEL_PATH = os.path.join(
+          self.REPO_PATH, "data", "models"
+      )
+
+      #
+      PATH_HUGGING_FACE = os.path.join(MODEL_REPO_PATH, "huggingface")
+      PATH_MOTION_MODULE = os.path.join(MODEL_REPO_PATH, "motion-module")
+      PATH_SD = os.path.join(MODEL_REPO_PATH, "sd")
+      PATH_DWPose = os.path.join(MODEL_REPO_PATH, "DWPose")
+      PATH_DWPose_DW11 = os.path.join(PATH_DWPose, "dw-ll_ucoco_384.onnx")
+
+      #
+      for model in [
+          PATH_HUGGING_FACE,
+          PATH_MOTION_MODULE,
+          PATH_SD,
+          PATH_DWPose,
+          PATH_DWPose_DW11
+      ]:
+        # copy from [Repo Parent]/models to [Repo Parent]/[Repo]/data/models
+        model_base_path = os.path.join(MODEL_REPO_PATH, model)
+        model_new_path = os.path.join(MODEL_PATH, model)
+
+        #
+        shutil.copytree(model_base_path, model_new_path, dirs_exist_ok=True)
+
+  # set config
+  def set_config(self, filepath):
+      #
+      self.config_json_path = filepath
+
+      #
+      try:
+        with open(self.config_json_path, 'r') as json_file:  # Open the file
+            self.config = json.load(json_file)  # Read and convert JSON data into Python object
+
+        #
+        print("Read config success")
+
+      except Exception as e:
+        print("Config Read error:", e)
+
+  # update config
+  def update_config(self, var_dict):
+      #
+      if self.config is None:
+        raise Exception("The config is not initialized yet")
+
+      for key, value in var_dict.items():
+          if key in self.config["controlnet_map"]:
+              self.config["controlnet_map"][key]["enable"] = str(value).lower()
+
+  def update_config_set_ytframes(self, downloaded_video_name, video_frames_dir):
+      # update control net image path
+      controlnet_image_path_video = os.path.join(
+          self.controlnet_images_path,
+          downloaded_video_name
+      )
+      self.config["controlnet_map"]["input_image_dir"] = controlnet_image_path_video
+
+
+      # create directory for each enabled control
+      for cntrl in var_dict:
+        if var_dict[cntrl]:
+          cntrl_dir_path = os.path.join(
+            controlnet_image_path_video,
+            cntrl
+          )
+          print(f"creating dir {cntrl_dir_path}")
+
+          # remote dir
+          shutil. rmtree(cntrl_dir_path)
+
+          # create dir
+          os.mkdir(cntrl_dir_path)
+
+          # copy all video frames to cntrl_dir
+          shutil.copy2(video_frames_dir, cntrl_dir_path)
+
+          print(f"copy from ${video_frames_dir} to ${cntrl_dir_path}")
