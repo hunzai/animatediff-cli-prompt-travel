15d3fe6fc951b2335ebed61879e9c9fe60cfdb63
Shawaiz <shawaiz.shah@lhg.lovehoneygroup.com>
2023-11-22 01:10:56 +0100

    Remove dot_env

--- src/minas/__init__.py
+++ src/minas/__init__.py
@@ -8,18 +8,18 @@ PACKAGE = __package__.replace("_", "-")
 PACKAGE_ROOT = Path(__file__).parent.parent
 
 #
-PROJECT_ROOT = os.getcwd()
+# PROJECT_ROOT = os.getcwd()
 
-# Path to your .env file
-dotenv_path = os.path.join(
-    PROJECT_ROOT,
-    '.env'
-)
+# # Path to your .env file
+# dotenv_path = os.path.join(
+#     PROJECT_ROOT,
+#     '.env'
+# )
 
-print("Reading .env file from", dotenv_path)
+# print("Reading .env file from", dotenv_path)
 
 # Load the .env file
-load_dotenv(dotenv_path)
+# load_dotenv(dotenv_path)
 
 #
 __all__ = [
